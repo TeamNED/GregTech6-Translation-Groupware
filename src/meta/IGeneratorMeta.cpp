046475f7eef9f0b005415ddc62796be6d40b2193
Tanimodori <unknowner2014@gmail.com>
2021-11-27 16:34:53 +0800

    Fix wrong namespace condition

--- src/meta/IGeneratorMeta.cpp
+++ src/meta/IGeneratorMeta.cpp
@@ -9,7 +9,7 @@ bool operator!=(const IGeneratorMeta &lhs, const IGeneratorMeta &rhs) {
 bool IGeneratorMeta::contains(const IGeneratorMeta &that) const {
   const string &this_ns = this->namespace_prefix();
   const string &that_ns = that.namespace_prefix();
-  if (this_ns.rfind(that_ns, 0) == 0) {
+  if (that_ns.rfind(this_ns, 0) == 0) {
     // this_ns is started with that_ns
     const auto &this_ext = this->extensions();
     const auto &that_ext = that.extensions();
